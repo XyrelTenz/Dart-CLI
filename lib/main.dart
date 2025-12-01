38733eb8961fd74b06e92dce88272b04061bd9bc
XyrelTenz <xdemocrito1@gmail.com>
2025-12-01 16:34:37 +0800

    DSA

--- lib/main.dart
+++ lib/main.dart
@@ -40,27 +40,3 @@ void main() {
   Vertex<dynamic> vertexes = Vertex<dynamic>(index: 1, data: 2);
   print(vertexes);
 }
-
-class Stack<T> {
-  final List<T> _storage = <T>[];
-
-  void push(T elements) {
-    _storage.add(elements);
-  }
-
-  T pop() {
-    if (_storage.isEmpty) throw Exception("Stack is Empty");
-    return _storage.removeLast();
-  }
-
-  T get peek {
-    if (_storage.isEmpty) throw Exception("Stack is Empty");
-    return _storage.last;
-  }
-
-  bool get isEmpty => _storage.isEmpty;
-  bool get isNotEmpty => _storage.isNotEmpty;
-
-  @override
-  String toString() => "Stack: $_storage";
-}
