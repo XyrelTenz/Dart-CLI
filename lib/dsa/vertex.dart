975f55c978eee29be44f4b3f87eda9a5ff5f1e6f
XyrelTenz <xdemocrito1@gmail.com>
2025-12-03 11:29:29 +0800

    Graph

--- lib/dsa/vertex.dart
+++ lib/dsa/vertex.dart
@@ -1,8 +1,8 @@
+// Graph Components
 class Vertex<T> {
   final int index;
   final T data;
 
-  //
   Vertex({required this.index, required this.data});
   @override
   String toString() => data.toString();
